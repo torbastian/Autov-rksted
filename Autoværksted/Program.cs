3e8a627125741f654e2db02075f418835c9de292
jordfolk <mj.elborough@hotmail.com>
2018-03-16 09:07:07 +0100

    Menu start i program.cs

--- Autoværksted/Program.cs
+++ Autoværksted/Program.cs
@@ -12,13 +12,50 @@ namespace Autoværksted
     {
         static void Main(string[] args)
         {
-            //Kunde k1 = new Kunde();
-            //k1.Fornavn = "Test";
-            //k1.Efternavn = "Tester";
-            //k1.Adresse = "Testvej 47";
-            //k1.Tlf = "12345678";
 
-            Datalag lag = new Datalag();
+
+
+
+
+
+            int updatekunde = 0;
+            Console.WriteLine("Her kan du skifte dine kunde oplysninger");
+            Console.WriteLine("Indtast dit Kunde id");
+
+            Console.WriteLine("\nTryk 1 for Fornavn, Tryk 2 for Efternavn, Tryk 3 for Adresse, Tryk 4 for Tlf Nr, Tryk 5 for at Anulere");
+            updatekunde = Convert.ToInt16(Console.ReadKey());
+
+            switch (updatekunde)
+            {
+                case 1:
+                    Console.WriteLine("Fornavn vil blive skiftet!");
+                    Console.Write("Hvad er dit nye Fornavn: ");
+                    break;
+                case 2:
+                    Console.WriteLine("Efternavn vil blive skiftet!");
+                    Console.Write("Hvad er dit nye Efternavn: ");
+                    break;
+                case 3:
+                    Console.WriteLine("Adresse vil blive skiftet!");
+                    Console.Write("Hvad er din nye Adresse: ");
+                    break;
+                case 4:
+                    Console.WriteLine("Telefon Nr. vil blive skiftet!");
+                    Console.Write("Hvad er dit nye tlf Nr: ");
+                    break;
+                default:
+                    Console.WriteLine("Default case");
+                    break;
+            }
+
+
+            //Kunde k4 = new Kunde();
+            //k4.Fornavn = "Test";
+            //k4.Efternavn = "Tester";
+            //k4.Adresse = "Testvej 47";
+            //k4.Tlf = "12345678";
+            //Console.WriteLine(k4.Fornavn + k4.Efternavn + k4.Adresse + k4.Tlf);
+            //Datalag lag = new Datalag();
             //lag.CreateKunde(k1);
             //Datalag lag = new Datalag();
             //Bil b1 = new Bil();
@@ -34,7 +71,7 @@ namespace Autoværksted
             //Bil b2 = lag.hentBil("xx12345");
             //Console.WriteLine(b2.Model);
 
-            Console.ReadKey();
+            //Console.ReadKey();
         }
     }
 }
