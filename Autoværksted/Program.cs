8deb9d39c1f3369646d0cf556ecb517776db6e06
jordfolk <mj.elborough@hotmail.com>
2018-03-16 10:25:12 +0100

    hfd

--- Autoværksted/Program.cs
+++ Autoværksted/Program.cs
@@ -10,43 +10,99 @@ namespace Autoværksted
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+            Datalag lag = new Datalag();
 
-
-
-
-
-
-            int updatekunde = 0;
-            Console.WriteLine("Her kan du skifte dine kunde oplysninger");
-            Console.WriteLine("Indtast dit Kunde id");
-
-            Console.WriteLine("\nTryk 1 for Fornavn, Tryk 2 for Efternavn, Tryk 3 for Adresse, Tryk 4 for Tlf Nr, Tryk 5 for at Anulere");
-            updatekunde = Convert.ToInt16(Console.ReadKey());
-
-            switch (updatekunde)
+            Console.WriteLine("Dette program kan Dette program kan oprette, slette og updatere en Kunde database og en Bil database\nSamt beregne hvornår bilen har været/skal på værksted");
+            
+            int hvadvildu = 0, kunder = 0, biler = 0, vaerksted = 0;
+            Console.WriteLine("\nTryk 1 for Kunder\nTryk 2 for Biler\nTryk 3 for Værksted");
+            hvadvildu = Convert.ToInt16(Console.ReadLine());
+            switch (hvadvildu)
             {
                 case 1:
-                    Console.WriteLine("Fornavn vil blive skiftet!");
-                    Console.Write("Hvad er dit nye Fornavn: ");
+                    Console.WriteLine("Du har valgt Kunder");
+                    kunder = Convert.ToInt16(Console.ReadLine());
+                    switch(kunder)
+                    {
+                        case 1:
+                            Console.WriteLine("Opretter ny Kunde");
+                            
+                            break;
+                        case 2:
+                            Console.WriteLine("Her kan du skifte dine kunde oplysninger");
+                            Console.Write("Indtast dit Kunde id: ");
+
+                            break;
+                        case 3:
+                            Console.WriteLine("Sletter kunde");
+                            Console.WriteLine("\nIndtast kunde ud fra Kundens id");
+                            break;
+                        case 4:
+                            Console.WriteLine("Kundeoversigt");
+                            lag.ShowKunde();
+                            break;
+                        case 5:
+                            Console.WriteLine("Kundeoversigt");
+                            //lag.ShowKundeAll();
+                            break;
+                        default:
+                            break;
+                    }
+
+
                     break;
                 case 2:
-                    Console.WriteLine("Efternavn vil blive skiftet!");
-                    Console.Write("Hvad er dit nye Efternavn: ");
+                    Console.WriteLine("Du har valgt Biler");
+                    switch (biler)
+                    {
+                        case 1:
+                            Console.WriteLine();
+                            break;
+                        case 2:
+                            Console.WriteLine();
+                            break;
+                        case 3:
+                            Console.WriteLine();
+                            break;
+                        case 4:
+                            Console.WriteLine();
+                            break;
+                        default:
+                            break;
+                    }
+
+
                     break;
                 case 3:
-                    Console.WriteLine("Adresse vil blive skiftet!");
-                    Console.Write("Hvad er din nye Adresse: ");
-                    break;
-                case 4:
-                    Console.WriteLine("Telefon Nr. vil blive skiftet!");
-                    Console.Write("Hvad er dit nye tlf Nr: ");
+                    Console.WriteLine("Du har valgt Værksted");
+                    switch (vaerksted)
+                    {
+                        case 1:
+                            Console.WriteLine();
+                            break;
+                        case 2:
+                            Console.WriteLine();
+                            break;
+                        case 3:
+                            Console.WriteLine();
+                            break;
+                        case 4:
+                            Console.WriteLine();
+                            break;
+                        default:
+                            break;
+                    }
+
+
+
                     break;
                 default:
-                    Console.WriteLine("Default case");
                     break;
             }
+
 
 
             //Kunde k4 = new Kunde();
@@ -57,7 +113,7 @@ namespace Autoværksted
             //Console.WriteLine(k4.Fornavn + k4.Efternavn + k4.Adresse + k4.Tlf);
             //Datalag lag = new Datalag();
             //lag.CreateKunde(k1);
-            //Datalag lag = new Datalag();
+
             //Bil b1 = new Bil();
 
             //b1.RegNR = "xx12345";
@@ -71,7 +127,10 @@ namespace Autoværksted
             //Bil b2 = lag.hentBil("xx12345");
             //Console.WriteLine(b2.Model);
 
-            //Console.ReadKey();
+
+
+
+            Console.ReadKey();
         }
     }
 }
