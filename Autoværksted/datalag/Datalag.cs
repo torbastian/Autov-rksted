3f20ae497b7eb6152223ae943cca2fe2081ab796
jordfolk <mj.elborough@hotmail.com>
2018-03-19 10:23:07 +0100

    Gør det nemmere at finde hvor kunder,biler og værksted starter i datalag

--- Autoværksted/datalag/Datalag.cs
+++ Autoværksted/datalag/Datalag.cs
@@ -8,7 +8,7 @@ namespace Autoværksted
 {
     class Datalag
     {
-        //Kunder
+        //Kunder <-<-<-<-<-<-<-<-<-
         public void CreateKunde()
         {   //får fat i kunde info
             Kunde kunde = new Kunde();
@@ -263,7 +263,7 @@ namespace Autoværksted
             }
         }
 
-        //Biler
+        //Biler <-<-<-<-<-<-<-<-<-
         public void CreateBil()
         {   //Opret ny bil
             Bil bil = new Bil();
@@ -545,7 +545,7 @@ namespace Autoværksted
 
                 List<string> ToUpdate = new List<string>();
                 
-                //Find ud af hvad der skal ændres
+                //Dette finder ud af hvad der skal ændres
                 if (!string.IsNullOrEmpty(bil.RegNR))
                     ToUpdate.Add(string.Format("reg_nr = '{0}'", bil.RegNR));
 
@@ -586,7 +586,7 @@ namespace Autoværksted
             }
         }
 
-        //Autoværksted
+        //Autoværksted <-<-<-<-<-<-<-<-<-<-
         public void NewAutoRecord()
         {   //Opret ny værksteds log
             Console.Write("Opret nyt værksteds besøg\nIndtast Kunde id: ");
