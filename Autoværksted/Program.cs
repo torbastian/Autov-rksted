445008d8343a9089bdfb1e979d171326c6a30127
jordfolk <mj.elborough@hotmail.com>
2018-03-19 09:55:03 +0100

    Opdatere i program.cs er nu med O i stedet for U

--- Autoværksted/Program.cs
+++ Autoværksted/Program.cs
@@ -62,7 +62,7 @@ namespace Autoværksted
             int kunder = 0;
             Console.WriteLine("Du har valgt Kunder");
             Console.WriteLine("\nIndtast 1 for at oprette en Kunde\n" +
-                                "Indtast 2 for at Updatere oplysninger på Kunde\n" +
+                                "Indtast 2 for at Opdatere oplysninger på Kunde\n" +
                                 "Indtast 3 for at slette en Kunde\n" +
                                 "Indtast 4 for at vise oplysninger af Kunde\n" +
                                 "Indtast 5 for vise alle Kunder");
@@ -120,7 +120,7 @@ namespace Autoværksted
             int biler = 0;
             Console.WriteLine("Du har valgt Biler");
             Console.WriteLine("\nIndtast 1 for at oprette en Bil\n" +
-                                "Indtast 2 for at Updatere oplysninger på Bil\n" +
+                                "Indtast 2 for at Opdatere oplysninger på Bil\n" +
                                 "Indtast 3 for at slette en Bil\nIndtast 4 for at vise oplysninger af bil\n" +
                                 "Indtast 5 for vise alle Biler");
 
@@ -136,7 +136,7 @@ namespace Autoværksted
                     break;
 
                 case 2:
-                    Console.WriteLine("\nHer updatere du din Bils oplysninger");
+                    Console.WriteLine("\nHer Opdatere du din Bils oplysninger");
                     lag.UpdateBil(); //Bruger metoden UpdateBil fra Klassen Datalag
                     break;
 
