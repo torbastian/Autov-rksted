bafb1b554b73c1dbc563e51bd7f6a2e2f617d781
Tor-bastian Hansen <torbastianhansen@hotmail.com>
2018-03-20 13:33:29 +0100

    AutoVærksted SQL

--- Autoværksted/sql/Autoværksted.sql
+++ Autoværksted/sql/Autoværksted.sql
@@ -28,7 +28,12 @@ oprettelsesdato date not null
 
 create table Vaerkstedsophold (
 id int identity(1,1) Primary key,
-dato date not null,
+hentning_dato date,
+aflevring_dato date not null,
 kunde_id int foreign key references Kunder(id),
-fk_reg_nr nvarchar(8) foreign key references Biler(reg_nr)
+fk_reg_nr nvarchar(8) foreign key references Biler(reg_nr),
+diagnose nvarchar(500),
+kunde_kommentar nvarchar(500),
+skade nvarchar(500)
 )
+
