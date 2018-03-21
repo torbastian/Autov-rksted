0eca82c448bf00b84c8b3e171864af180e4cacea
Tor-bastian Hansen <torbastianhansen@hotmail.com>
2018-03-21 14:02:17 +0100

    Final2 SQL Opdateret

--- Autoværksted/sql/Autoværksted.sql
+++ Autoværksted/sql/Autoværksted.sql
@@ -1,7 +1,9 @@
 use master
 go
-create database AutoVaerksted
+
+Create database AutoVaerksted
 go
+
 use AutoVaerksted
 go
 
@@ -13,6 +15,7 @@ adresse nvarchar(50) not null,
 tlf nvarchar(12),
 oprettelsesdato date not null
 )
+go
 
 create table Biler (
 reg_nr nvarchar(8) primary key,
@@ -25,11 +28,12 @@ braendstof nvarchar(30) not null,
 km_l float not null,
 oprettelsesdato date not null
 )
+go
 
 create table Vaerkstedsophold (
 id int identity(1,1) Primary key,
 oprettelsesdato date not null,
-aflevring_dato date,
+aflevering_dato date,
 hentning_dato date,
 kunde_id int foreign key references Kunder(id),
 fk_reg_nr nvarchar(8) foreign key references Biler(reg_nr),
@@ -37,4 +41,20 @@ kunde_kommentar nvarchar(500),
 diagnose nvarchar(500),
 skade nvarchar(500)
 )
+go
 
+
+insert into Kunder (oprettelsesdato, fornavn, efternavn, adresse, tlf) Values 
+(GETDATE(), 'Børge', 'Bensen', 'Bovej 22', '27212233'),
+(GETDATE(), 'Claus', 'Petersen', 'Stevej 110', '33446771')
+go
+
+Insert into Biler (oprettelsesdato, kunde_id, reg_nr, maerke, model, km, km_l, braendstof, aargang) values
+(GETDATE(), 1, 'XXXABC', 'Tesla', 'S', 15000, 33.2, 'El', 2015),
+(GETDATE(), 2, 'ABCDEF', 'Ford', 'Mustang', 200, 7.5, 'Diesel', 2014),
+(GETDATE(), 2, 'FEDCBA', 'Ford', 'Fiesta', 200000, 16.4, 'Benzin', 2010)
+go
+
+insert into Vaerkstedsophold (oprettelsesdato, aflevering_dato, hentning_dato, kunde_id, fk_reg_nr, kunde_kommentar, diagnose, skade) values
+(GETDATE(), null, null, 2, 'ABCDEF', 'Ryger', 'Skadet motor', 'Ny Motor')
+go
